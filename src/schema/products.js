e45b91ef6a80fd500699444562e60a4ca4da9859
quangph22821 <118173820+quangph22821@users.noreply.github.com>
2023-07-25 23:00:01 +0700

    update

--- src/schema/products.js
+++ src/schema/products.js
@@ -5,8 +5,8 @@ export const productSchema = Joi.object({
     price:Joi.number().required(),
     description:Joi.string().required(),
     image:Joi.string().required(),
-    category_Id: Joi.string().required(),
-    size:Joi.array(),
-    color:Joi.array(),
-    quantity:Joi.number().required()
+    category_Id: Joi.array().required(),
+    // size:Joi.array(),
+    // color:Joi.array(),
+    // quantity:Joi.number().required()
 })
